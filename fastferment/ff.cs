d3f7d16ca902723c2702476be2e0f4d4ccd27606
Fang86 <fangodus@gmail.com>
2021-03-09 22:27:21 -0700

    Initial commit

---
+++ fastferment/ff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+
+namespace fastferment
+{
+    [BepInPlugin(MID, NAME, VERSION)]
+    internal class FastFerment : BaseUnityPlugin
+    {
+        private const string MID = "valheim.mod.fastferment";
+        private const string NAME = "Fast Ferment";
+        private const string VERSION = "1.0.0.0";
+        private const string author = "Fang86";
+
+        private static ConfigFile configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "fastferment.cfg"), true);
+        private static ConfigEntry<float> duration = configFile.Bind("General", "duration", 1f, "The amount of time it takes for the fermenter to ferment");
+
+        void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+
+            Logger.LogInfo($"Fast Ferment mod - Created by {author}");
+        }
+
+        [HarmonyPatch(typeof(Fermenter), "Awake")]
+        public static class Patcher
+        {
+            private static void Prefix(ref Fermenter __instance)
+            {
+                __instance.m_fermentationDuration = FastFerment.duration.Value;
+            }
+        }
+    }
+}
